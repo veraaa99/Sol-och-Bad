9a17574e9b273538eaa2283030914ad9ef117365
Vera <althinivera77@gmail.com>
2024-12-16 11:29:37 +0100

    Started working on a shopping cart function in Javscript

---
+++ script.js
@@ -0,0 +1,31 @@
+// Varukorgsfunktion
+const savedTrips = []
+let addToCart 
+let newTrip = ""
+
+addToCart.addEventListener('change', e => {
+    if (e.target.checked) {
+        delete todos[newTrip]
+    } else {
+        savedTrips.push(newTrip)
+    }
+})
+
+// Bokningsfunktion
+
+// Formulär på frågesidan
+
+// FAQ (klicka på knapp så man ser svaret på frågan)
+
+// Prenumerera på nyhetsbrev
+
+
+
+// Filtrera
+
+// Lämna och skapa recensioner
+
+
+// Favoriter
+
+// Modal popup 
