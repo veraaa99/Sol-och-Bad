d02ec73b076695fe5841da123c5b7774319b5d2f
Vera <althinivera77@gmail.com>
2024-12-17 12:32:46 +0100

    Implemented feature to add trips to shopping cart (heart). Added logic for a notification icon when user has added trips to shopping cart.

--- script.js
+++ script.js
@@ -1,22 +1,43 @@
-// Varukorgsfunktion
+// Shopping cart feature
+// Variables for shopping cart and list of all heart buttons (add to cart-buttons)
 let shoppingCart = document.querySelector('#cart')
-let heartButtons = document.querySelectorAll('svg')
+let heartButtonsImage = document.querySelectorAll('svg')
+let heartButtonsList = Object.values(heartButtonsImage);
 
+// Placeholder text for notification function
 const testText = document.createElement("p")
 testText.innerText = "This is a paragraph";
 document.body.appendChild(testText);
 testText.style.display = "none"
 
-heartButtons.forEach(heart => {
+function checkSavedTrips(heart){
+    return heart.querySelector('path').getAttribute("fill") === "black"
+}
+
+function showOrHideNotification() {
+    if(heartButtonsList.every(checkSavedTrips)){
+        testText.style.display = "none"
+        return false
+    } else {
+        testText.style.display = "block"
+        return true
+    }
+}
+
+heartButtonsList.forEach(heart => {
     heart.addEventListener('click', () => {
         if (heart.querySelector('path').getAttribute("fill") === "black") {
             heart.querySelector('path').setAttribute("fill", "red")
-            testText.style.display = "block"
+            showOrHideNotification()
         } else {
             heart.querySelector('path').setAttribute("fill", "black")
-    }
+            showOrHideNotification()
+        }
+    })
 })
-})
+
+// Export to test file
+module.exports = {checkSavedTrips, showOrHideNotification, heartButtonsList}
 
 // Bokningsfunktion
 
