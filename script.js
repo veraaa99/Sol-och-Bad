156492183c299f6d0bad37b9bc90a4467fc9c743
Rebecka <rebecka.rl.larsson@gmail.com>
2024-12-17 10:17:06 +0100

    Send message button functioning

--- script.js
+++ script.js
@@ -21,11 +21,38 @@ heartButtons.forEach(heart => {
 // Bokningsfunktion
 
 // Formulär på frågesidan
+if (document.querySelector('#formBtn')) {
+    const formBtn = document.getElementById('formBtn');
+    formBtn.addEventListener('click', (e) => {
+        e.preventDefault();
+        displayThanks();
+        })
+};
+
+const displayThanks = function() {
+    const thanksText = document.createElement("p");
+    thanksText.innerText = "Tack för ditt meddelande, vi hör av oss!";
+    const messageUs = document.getElementById('messageUs');
+    messageUs.appendChild(thanksText);
+};
 
 // FAQ (klicka på knapp så man ser svaret på frågan)
 
 // Prenumerera på nyhetsbrev
+// if (document.querySelector('#subscribeBtn')) {
+//     const formBtn = document.getElementById('subscribeBtn');
+//     formBtn.addEventListener('click', (e) => {
+//         e.preventDefault();
+//         displayThanks();
+//         })
+// };
 
+// const displaySubscribed = function() {
+//     const subscribedText = document.createElement("p");
+//     subscribedText.innerText = "Välkommen! Du blir först i att ta del av spännande erbjudaden.";
+//     const subscribe = document.getElementById('subscribe');
+//     subscribe.appendChild(subscribedText);
+// };
 
 
 // Filtrera
