1ee6e925b62d806893c781f52fcc0e0db17e3917
Rebecka <rebecka.rl.larsson@gmail.com>
2024-12-17 15:31:49 +0100

    Subscribe button and Send message button tested and passed test (should display message when button is clicked)

--- script.js
+++ script.js
@@ -1,58 +1,82 @@
-// Varukorgsfunktion
+// Shopping cart feature
+// Variables for shopping cart and list of all heart buttons (add to cart-buttons)
 let shoppingCart = document.querySelector('#cart')
-let heartButtons = document.querySelectorAll('svg')
+let heartButtonsImage = document.querySelectorAll('svg')
+let heartButtonsList = Object.values(heartButtonsImage);
 
+// Placeholder text for notification function
 const testText = document.createElement("p")
 testText.innerText = "This is a paragraph";
 document.body.appendChild(testText);
 testText.style.display = "none"
 
-heartButtons.forEach(heart => {
+function checkSavedTrips(heart){
+    return heart.querySelector('path').getAttribute("fill") === "black"
+}
+
+function showOrHideNotification() {
+    if(heartButtonsList.every(checkSavedTrips)){
+        testText.style.display = "none"
+        return false
+    } else {
+        testText.style.display = "block"
+        return true
+    }
+}
+
+heartButtonsList.forEach(heart => {
     heart.addEventListener('click', () => {
         if (heart.querySelector('path').getAttribute("fill") === "black") {
             heart.querySelector('path').setAttribute("fill", "red")
-            testText.style.display = "block"
+            showOrHideNotification()
         } else {
             heart.querySelector('path').setAttribute("fill", "black")
-    }
+            showOrHideNotification()
+        }
+    })
 })
-})
+
+// Export to test file
+// module.exports = {checkSavedTrips, showOrHideNotification, heartButtonsList}
 
 // Bokningsfunktion
 
 // Formulär på frågesidan
-if (document.querySelector('#formBtn')) {
-    const formBtn = document.getElementById('formBtn');
-    formBtn.addEventListener('click', (e) => {
-        e.preventDefault();
-        displayThanks();
-        })
-};
-
-const displayThanks = function() {
-    const thanksText = document.createElement("p");
-    thanksText.innerText = "Tack för ditt meddelande, vi hör av oss!";
+function displayThanks(document) {
     const messageUs = document.getElementById('messageUs');
+    const thanksText = document.createElement('p');
+    thanksText.setAttribute('id', 'thankUser');
+    thanksText.innerText = "Tack för ditt meddelande, vi hör av oss!";
     messageUs.appendChild(thanksText);
-};
+    return true;
+  };
+  
+const formBtn = document.getElementById('formBtn');
+    if (formBtn) {
+        formBtn.addEventListener('click', () => displayThanks(document));
+    };
+  
+  module.exports = {displayThanks};
+  
 
 // FAQ (klicka på knapp så man ser svaret på frågan)
 
 // Prenumerera på nyhetsbrev
-// if (document.querySelector('#subscribeBtn')) {
-//     const formBtn = document.getElementById('subscribeBtn');
-//     formBtn.addEventListener('click', (e) => {
-//         e.preventDefault();
-//         displayThanks();
-//         })
-// };
+function displaySubscribed(document) {
+    const subscribe = document.getElementById('subscribe');
+    const subscribedText = document.createElement('p');
+    subscribedText.innerText = "Välkommen! Du blir först med att ta del av nya och spännande erbjudanden.";
+    subscribe.appendChild(subscribedText);
+    return true;
+  };
 
-// const displaySubscribed = function() {
-//     const subscribedText = document.createElement("p");
-//     subscribedText.innerText = "Välkommen! Du blir först i att ta del av spännande erbjudaden.";
-//     const subscribe = document.getElementById('subscribe');
-//     subscribe.appendChild(subscribedText);
-// };
+const subscribeBtn = document.getElementById('subscribeBtn');
+    if (subscribeBtn) {
+      subscribeBtn.addEventListener('click', () => displaySubscribed(document));
+    };
+  
+  
+  module.exports = {displaySubscribed};
 
 
 // Filtrera
